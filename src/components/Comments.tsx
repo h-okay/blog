860fa50addcc3d02b72b40da6ebcef877e133b55
Hakan Okay <hokay.ca@gmail.com>
2024-08-27 16:52:32 -0400

    Update site_id

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { theme } from "../../tailwind.config.cjs";
 
 declare global {
@@ -24,7 +24,7 @@ const insertScript = (id: string, parentElement: HTMLElement) => {
   script.innerHTML = `
     var remark_config = {
       host: "${host}",
-      site_id: "remark",
+      site_id: "blog.hakanokay.dev",
       url: "${url}",
       theme: "dark",
       components: ["counter", "embed"],
