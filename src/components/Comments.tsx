bb5b94f30888f4db1f229a8ef230921734447273
Hakan Okay <hokay.ca@gmail.com>
2024-08-27 19:31:00 -0400

    Test disqus

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,58 +1,31 @@
-import { useEffect, useState } from "react";
+import { CommentCount, DiscussionEmbed } from "disqus-react";
 
-export default function Comments() {
-  const [scriptLoaded, setScriptLoaded] = useState(false);
+type CommentsProps = {
+  url: string;
+};
 
-  useEffect(() => {
-    const loadIssoScript = () => {
-      return new Promise((resolve, reject) => {
-        const script = document.createElement("script");
-        script.type = "text/javascript";
-        script.src = "https://isso.fly.dev/js/embed.min.js";
-        script.setAttribute("data-isso", "https://isso.fly.dev/");
-        script.setAttribute("data-isso-css", "true");
-        script.async = true;
-
-        script.onload = () => resolve(true);
-        script.onerror = () => reject(false);
-
-        document.head.appendChild(script);
-      });
-    };
-
-    loadIssoScript()
-      .then(() => setScriptLoaded(true))
-      .catch(error => console.error("Failed to load Isso script:", error));
-  }, []);
-
+export default function Comments({ url }: CommentsProps) {
   return (
     <section id="comments" className="w-full p-0">
-      <h2 className="pb-5 text-xl font-semibold">Comments</h2>
-      {!scriptLoaded && (
-        <div id="loading-animation">
-          <svg
-            className="text-gray-500 h-5 w-5 animate-spin self-center"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle
-              className="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              strokeWidth="4"
-            />
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            />
-          </svg>
-        </div>
-      )}
-      <div id="isso-thread"></div>
+      <CommentCount
+        shortname="blog-hakanokay-dev"
+        config={{
+          url: url,
+          identifier: url,
+          title: url,
+        }}
+      >
+        <h2 className="pb-5 text-xl font-semibold">Comments</h2>
+      </CommentCount>
+      <DiscussionEmbed
+        shortname="blog-hakanokay-dev"
+        config={{
+          url: url,
+          identifier: url,
+          title: url,
+          language: "en_US",
+        }}
+      />
     </section>
   );
 }
